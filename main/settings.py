25a2c8935c8eec2f45d2df7953093c389943ca54
unknown <sofiareyesflores8@gmail.com>
2018-03-22 17:40:26 -0500

    initial commit

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.review',
     'apps.login',
     'apps.booksandauthors',
     'apps.courses',
--- main/urls.py
+++ main/urls.py
@@ -21,5 +21,5 @@ from django.contrib import admin
 # ]
 
 urlpatterns=[
-    url(r'^', include('apps.login.urls'))
+    url(r'^', include('apps.review.urls'))
 ]
